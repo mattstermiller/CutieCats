a29f5e33d1f48249471089cc145ecbbfe69f3b15
Matt Miller <miller.mattster@gmail.com>
2024-03-01 22:05:22 -0600

    Initial commit with bootstrapped monogame project

---
+++ CutieCats/Program.fs
@@ -0,0 +1,26 @@
+﻿namespace SuperPong
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics;
+open Microsoft.Xna.Framework.Input;
+
+type CutieCatsGame() as this = 
+    inherit Game()
+
+    let graphics = new GraphicsDeviceManager(this)
+
+    override __.LoadContent() = 
+        ()
+
+    override __.Update(gameTime) =
+        ()
+
+    override __.Draw(gameTime) =
+        this.GraphicsDevice.Clear Color.Black
+
+module Entry =
+    [<EntryPoint>]
+    let main _ =
+        use game = new CutieCatsGame()
+        game.Run()
+        0
