650ca0fba56a92092f3547ee8bed0d2e8aa3876a
Matt Miller <miller.mattster@gmail.com>
2024-03-28 11:55:37 -0500

    Adds collision detection, mouse ship destruction and game reset

--- CutieCats/Actors.fs
+++ CutieCats/Actors.fs
@@ -32,6 +32,10 @@ type Projectile(initPos: Vector2, vel: Vector2) =
 
     let radius = size/2f
 
+    member _.IsFacingEnemy = vel.X > 0f
+    member _.Rect = RectangleF.ofPosSize(pos, size)
+    member _.Damage = 0.12f
+
     interface IActor with
         member _.Update elapsedSec =
             pos <- pos + vel * elapsedSec
@@ -56,15 +60,20 @@ type Weapon(getFirePos, vel) =
 
 type CatShip() =
     let size = Size2(0.08f, 0.08f)
-    let mutable pos = Vector2(0.2f, 0.5f)
-    let mutable dir = Vector2.Zero
+    let initPos = Vector2(0.2f, 0.5f)
+    let mutable pos = initPos
     let speed = 0.25f
+    let mutable dir = Vector2.Zero
 
     let radius = size/2f
     let posMin = radius.ToVector2()
     let posMax = Vector2.One - posMin
 
     let getFirePos () = pos + Vector2(size.Width / 2f, 0f)
+
+    member _.Reset () =
+        pos <- initPos
+        dir <- Vector2.Zero
 
     member val Weapon = Weapon(getFirePos, Vector2(0.2f, 0f))
 
@@ -85,12 +94,24 @@ type CatShip() =
 
 type MouseShip(catShip: CatShip) =
     let size = Size2(0.10f, 0.12f)
-    let mutable pos = Vector2(0.8f, 0.5f)
+    let initPos = Vector2(0.85f, 0.5f)
+    let mutable pos = initPos
     let speed = 0.1f
+    let mutable health = 1f
 
     let radius = size/2f
     let posMin = radius.ToVector2()
     let posMax = Vector2.One - posMin
+
+    member _.Reset () =
+        pos <- initPos
+        health <- 1f
+
+    member _.Rect = RectangleF.ofPosSize(pos, size)
+
+    member _.Hit damage =
+        health <- health - damage
+        health < 0f
 
     interface IActor with
         member _.Update elapsedSec =
@@ -103,17 +124,36 @@ type MouseShip(catShip: CatShip) =
 
         member _.Draw viewport spriteBatch =
             spriteBatch.DrawEllipse(viewport.GetScreenPos(pos), viewport.GetScreenSize(radius), 20, Color.Gray, 30f)
+            // health bar
+            let margin = 0.02f
+            let maxWidth = 0.2f
+            let height = 0.03f
+            let barPos = viewport.GetScreenPos(Vector2(1.0f - margin - maxWidth, 1.0f - margin))
+            let barSize = viewport.GetScreenSize(Vector2(maxWidth * health, height))
+            spriteBatch.DrawRectangle(RectangleF(barPos, barSize), Color.Red, barSize.Height)
 
 type GameState(exitFunc) =
+    let gameResetTime = 4f
+
     let stars = Array.init 100 (fun _ -> Star())
     let catShip = CatShip()
     let mouseShip = MouseShip(catShip)
+    let mutable gameResetTimer = None
 
-    let mutable actors = [
-        yield! stars |> Seq.cast<IActor>
-        catShip
-        mouseShip
-    ]
+    let mutable actors = []
+
+    let init () =
+        actors <- [
+            yield! stars |> Seq.cast<IActor>
+            catShip
+            mouseShip
+        ]
+        catShip.Reset()
+        mouseShip.Reset()
+        gameResetTimer <- None
+
+    do
+        init ()
 
     member _.HandleInput (input: InputEvent) =
         match input with
@@ -125,15 +165,31 @@ type GameState(exitFunc) =
             exitFunc()
 
     member _.Update elapsedSec =
+        // update actors and add new ones
         let newActors = actors |> List.collect (fun actor -> actor.Update elapsedSec)
-
-        // TODO: collision detection and response
-        //      can remove actors
-        //      probably doesn't need to consider newly created actors
-        //      iterate over projectiles checking for collision with ships, shield, or no longer intersects world rect
-        // let projectiles = actors |> Seq.choose (function :? Projectile as p -> Some p | _ -> None)
-
         actors <- actors @ newActors
+
+        match gameResetTimer with
+        | Some time ->
+            // game over
+            let remaining = time - elapsedSec
+            if remaining > 0f then
+                gameResetTimer <- Some remaining
+            else
+                init ()
+        | None ->
+            // collisions
+            let destroyed = ResizeArray<IActor>()
+            actors
+            |> List.choose (function :? Projectile as p -> Some p | _ -> None)
+            |> List.iter (fun proj ->
+                if proj.IsFacingEnemy && mouseShip.Rect.Intersects(proj.Rect) then
+                    destroyed.Add proj
+                    if mouseShip.Hit proj.Damage then
+                        destroyed.Add mouseShip
+                        gameResetTimer <- Some gameResetTime
+            )
+            actors <- actors |> List.except destroyed
 
     member _.Draw viewport spriteBatch =
         actors |> List.iter (fun a -> a.Draw viewport spriteBatch)
--- CutieCats/Util.fs
+++ CutieCats/Util.fs
@@ -13,6 +13,10 @@ type Size2 with
     member this.ToVector2 () = Vector2(this.Width, this.Height)
     member this.Scale (scale: Vector2) = Size2(this.Width * scale.X, this.Height * scale.Y)
 
+type RectangleF with
+    static member ofPosSize(pos: Vector2, size: Size2) =
+        RectangleF(Point2(pos.X, pos.Y) - size/2f, size)
+
 type Signal<'a>(initialValue: 'a) =
     let evt = Event<'a>()
     let obs = evt.Publish
